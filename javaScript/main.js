1c8aab6ab2bebb3dcb03b4103bf777e2d82aed6e
DenimCity <altidorj1@gmail.com>
2017-12-11 18:06:31 -0500

    updated html, JS, and css

---
+++ javaScript/main.js
@@ -0,0 +1,20 @@
+let value1 = 0;
+let value2 = 0;
+let value3 = 0;
+
+
+let firstAnswer = prompt("What is 100 divide 2?);
+
+if (firstAnswer === 3) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+}
+
+
+
+const totalNumbers = (value1, value2, value3) {
+  submisionTotal = value1 + value2 +value3
+  console.log(submisionTotal);
+}
+totalNumber(100,200,300);
+console.log(totalNumber);
