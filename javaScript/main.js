9f2c48fc2ebdb2e4e67d9f35308fd110b6d73eda
DenimCity <altidorj1@gmail.com>
2017-12-11 22:29:53 -0500

    added new images to images folder, edit JS file

--- javaScript/main.js
+++ javaScript/main.js
@@ -4,66 +4,54 @@ $(document).ready(function(){
  
  alert("Welcome to GA Jeopardy")
  
-
-//  let value1 = 0;
-//  let value2 = 0;
-//  let value3 = 0;
-
-let secondAnswer = 200;
-let thirdAnswer = 7000;
- 
- 
- 
- /// ATTACH THIS TO THE FIRST BUTTON $100
+ /// first button prompts user amd records answer
  $( "button.firstQuestion" ).click(function() {
-  let firstAnswer = prompt( "What is 100 divide 2" );
+  let firstAnswer = prompt( "For $100: What is 100 divide 2?" );
   //needs to accept the user response and assign to  first answer
- 
 console.log(firstAnswer);
   if (firstAnswer === '50') {
-    //  document.getElementById("firstQuestion").innerHTML =
-      alert("Good Job");
+      alert("Good Job"); 
+      /// need to push the return value into an array
+      const num1=[];
+      let firstAnswer = num1[0];
+      
+    console.log(num1);
   } else {
     alert("Sorry, Try Again")
   }
 });
 
-//  $("firstQuestion").on('click', )
-//  let firstQuestion = prompt("What is 100 divide 2?");
-//  let firstAnswer = 50;
-//  if (firstAnswer === 50) {
-//     //  document.getElementById("firstQuestion").innerHTML =
-//      alert("Good Job");
-//  } else {
-//    alert("sorry that was the wrong answer")
-//  }
- 
- /// ATTACH THIS TO THE second BUTTON $100
- // let secondAnswer = prompt("What is 50 multipled by 4 ?");
- // if (secondAnswer === 3) {
-   
- //   document.getElementById("secondQuestion").innerHTML =
- //   "Hello " + person + "! How are you today?";
- // } else {
- // alert("sorry that was the wrong answer")
- 
- /// ATTACH THIS TO THE FIRST BUTTON $100
- 
- // let thirdAnswer = prompt("What is 100 divide 2?");
- // if (thirsAnswer === 3) {
- //   document.getElementById("thirdQuestion").innerHTML =
- //   "Hello " + person + "! How are you today?";
- // } else {
- // alert("sorry that was the wrong answer")
- 
- 
- // const totalNumbers = (value1, value2, value3) {
- //   submisionTotal = value1 + value2 +value3
- //   console.log(submisionTotal);
- // }
- // totalNumber(100,200,300);
- // console.log(` your totals score is ${totalNumber}`);
- 
+
+/// second button prompts user amd records answer
+$( "button.secondQuestion" ).click(function() {
+  let secondAnswer = prompt( "For $500: 5(-3x - 2) - (x - 3) = -4(4x + 5) + 13 " );
+  //needs to accept the user response and assign to  first answer
+console.log(secondAnswer);
+  if (secondAnswer === '0') {
+      alert("Good Job");
+    
+  } else {
+    alert("Sorry, Try Again")
+  }
+});
+
+
+
+/// third button prompts user amd records answer
+$( "button.thirdQuestion" ).click(function() {
+  let thirdAnswer = prompt( "For $1000: Solve this question [7 - (6 - 4(5 - 4) + 7)] + 4" );
+  //needs to accept the user response and assign to  first answer
+console.log(thirdsAnswer);
+  if (thirdAnswer === '2') {
+      alert("Good Job");
+    
+  } else {
+    alert("Sorry, Try Again")
+  }
+});
+
+
+
 });
 
 
