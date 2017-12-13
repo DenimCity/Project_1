28d522c71b931ba87ce1b50d956d0d71053a0cbe
DenimCity <altidorj1@gmail.com>
2017-12-13 05:38:34 -0500

    expanding grid

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,66 +1,96 @@
-$(document).ready(function(){
- console.log("The DOM is ready")
- 
- 
- alert("Welcome to GA Jeopardy")
-
- let newScore = 0
-
- /// first button prompts user amd records answer
- $( "div.button1" ).click(function() {
-  let firstAnswer = prompt( "For $100: What is 100 divide 2?" )
-  //needs to accept the user response and assign to  first answer
-  if (firstAnswer === '50') {
-     alert("Good Job")
-      /// need to push the return value into an array
-      newScore += 500
-      
-      console.log(newScore)
-  } else {
-    alert("Sorry, Try Again")
-    newScore -= 500
-    console.log(newScore)
-  }
-});
+$(document).ready(function () {
+  console.log("The DOM is ready")
 
 
-/// second button prompts user amd records answer
-$( "button.secondQuestion" ).click(function() {
-  let secondAnswer = prompt( "For $500: 5(-3x - 2) - (x - 3) = -4(4x + 5) + 13 " )
-  //needs to accept the user response and assign to  first answer
-console.log(secondAnswer)
-  if (secondAnswer === '0') {
+  alert("Welcome to GA Jeopardy")
+
+  let newScore = 0
+
+  $("div.button1").click(function () {
+    firstAnswer = prompt("For $100: What is 100 divide 2?")
+    if (firstAnswer === '50') {
       alert("Good Job")
-      let entry2 = 500
-      totalAnswers.push(entry2)
-      console.log(totalAnswers)
-  } else {
-    alert("Sorry, Lets Try Again")
-  }
-});
+      newScore += 100
+    } else {
+      alert("Sorry, Next Question")
+      newScore -= 100
+    }
+  });
 
 
+  $("div.button2").click(function () {
+    secondAnswer = prompt("For $100: 5(-3x - 2) - (x - 3) = -4(4x + 5) + 13 ")
+    if (secondAnswer === '0') {
+      alert("Good Job")
+      newScore += 100
+    } else {
+      alert("Sorry, Next Question")
+      newScore -= 100
+    }
+  });
 
-/// third button prompts user amd records answer
-$( "button.thirdQuestion" ).click(function() {
-  let thirdAnswer = prompt( "For $1000: How many solutions are there to the equation x2 - 7 = 0?" );
-  //needs to accept the user response and assign to  first answer
-console.log(thirdAnswer);
-  if (thirdAnswer === '1') {
+  $("div.button3").click(function () {
+    thirdAnswer = prompt("For $200: How many solutions are there to the equation x2 - 7 = 0?")
+    if (thirdAnswer === '1') {
       alert("Good Job");
-      let entry3 = 1000;
-      totalAnswers.push(entry3)
-      console.log(totalAnswers)
-  } else {
-    alert("Sorry, Try Again")
-  }
-});
+      newsScore += 200
+    } else {
+      alert("Sorry, Try Again")
+      newScore -= 200
+    }
+  });
+  $("div.button3").click(function () {
+    thirdAnswer = prompt("For $200: How many solutions are there to the equation x2 - 7 = 0?")
+    if (thirdAnswer === '1') {
+      alert("Good Job");
+      newsScore += 200
+    } else {
+      alert("Sorry, Try Again")
+      newScore -= 200
+    }
+  });
+  $("div.button3").click(function () {
+    thirdAnswer = prompt("For $200: How many solutions are there to the equation x2 - 7 = 0?")
+    if (thirdAnswer === '1') {
+      alert("Good Job");
+      newsScore += 200
+    } else {
+      alert("Sorry, Try Again")
+      newScore -= 200
+    }
+  });
+  $("div.button3").click(function () {
+    thirdAnswer = prompt("For $200: How many solutions are there to the equation x2 - 7 = 0?")
+    if (thirdAnswer === '1') {
+      alert("Good Job");
+      newsScore += 200
+    } else {
+      alert("Sorry, Try Again")
+      newScore -= 200
+    }
+  });
+  $("div.button3").click(function () {
+    thirdAnswer = prompt("For $200: How many solutions are there to the equation x2 - 7 = 0?")
+    if (thirdAnswer === '1') {
+      alert("Good Job");
+      newsScore += 200
+    } else {
+      alert("Sorry, Try Again")
+      newScore -= 200
+    }
+  });
+  $("div.button3").click(function () {
+    thirdAnswer = prompt("For $200: How many solutions are there to the equation x2 - 7 = 0?")
+    if (thirdAnswer === '1') {
+      alert("Good Job");
+      newsScore += 200
+    } else {
+      alert("Sorry, Try Again")
+      newScore -= 200
+    }
+  });
 
-const totalAnswers = []
-totalAnswers.forEach(()=>{
-
-})
-
+ 
 
 
 
