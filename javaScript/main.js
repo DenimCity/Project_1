6616ea15614c085020a9aea2dd034437787169c5
DenimCity <altidorj1@gmail.com>
2017-12-12 20:55:23 -0500

    html updates, css updated, JS updates

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,37 +1,39 @@
 $(document).ready(function(){
- console.log("The DOM is ready");
+ console.log("The DOM is ready")
  
  
  alert("Welcome to GA Jeopardy")
 
+ let newScore = 0
+
  /// first button prompts user amd records answer
- $( "button.firstQuestion" ).click(function() {
-  let firstAnswer = prompt( "For $100: What is 100 divide 2?" );
+ $( "div.button1" ).click(function() {
+  let firstAnswer = prompt( "For $100: What is 100 divide 2?" )
   //needs to accept the user response and assign to  first answer
-console.log(firstAnswer);
   if (firstAnswer === '50') {
-     alert("Good Job"); 
+     alert("Good Job")
       /// need to push the return value into an array
-      let entry1 = 50;
-      totalAnswers.push(entry1);
-      console.log(totalAnswers);
+      newScore += 500
+      
+      console.log(newScore)
   } else {
     alert("Sorry, Try Again")
-    
+    newScore -= 500
+    console.log(newScore)
   }
 });
 
 
 /// second button prompts user amd records answer
 $( "button.secondQuestion" ).click(function() {
-  let secondAnswer = prompt( "For $500: 5(-3x - 2) - (x - 3) = -4(4x + 5) + 13 " );
+  let secondAnswer = prompt( "For $500: 5(-3x - 2) - (x - 3) = -4(4x + 5) + 13 " )
   //needs to accept the user response and assign to  first answer
-console.log(secondAnswer);
+console.log(secondAnswer)
   if (secondAnswer === '0') {
-      alert("Good Job");
-      let entry2 = 500;
-      totalAnswers.push(entry2);
-      console.log(totalAnswers);
+      alert("Good Job")
+      let entry2 = 500
+      totalAnswers.push(entry2)
+      console.log(totalAnswers)
   } else {
     alert("Sorry, Lets Try Again")
   }
@@ -47,14 +49,14 @@ console.log(thirdAnswer);
   if (thirdAnswer === '1') {
       alert("Good Job");
       let entry3 = 1000;
-      totalAnswers.push(entry3);
-      console.log(totalAnswers);
+      totalAnswers.push(entry3)
+      console.log(totalAnswers)
   } else {
     alert("Sorry, Try Again")
   }
 });
 
-const totalAnswers = [];
+const totalAnswers = []
 totalAnswers.forEach(()=>{
 
 })
@@ -62,7 +64,7 @@ totalAnswers.forEach(()=>{
 
 
 
-});
+})
 
 
 
