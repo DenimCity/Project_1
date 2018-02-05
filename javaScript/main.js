446acd854a249d514b2b7cfa378a3840ae7fb84a
DenimCity <altidorj1@gmail.com>
2018-02-05 08:37:53 -0500

    questions refactored  and answers

--- javaScript/main.js
+++ javaScript/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 //score keeper
   let newScore = 0
   ///reset button
-  $('.realButton').click(function() {
+  $('.resetContainer').click(function() {
     location.reload();
 });
 
@@ -16,8 +16,8 @@ $(document).ready(function () {
   // }
   
   ///Button triggers
-  $("div.button1").click(function () {
-    let answer = prompt("For $100: 2 * 25 ?")
+  $("li.button1").click(function () {
+    let answer = prompt("For 100: 2 x 25 ?")
     if (answer === '50') {
       alert("Good Job")
       newScore += 100
@@ -26,12 +26,12 @@ $(document).ready(function () {
       newScore -= 100
     }
     $(".points").html(newScore);
-    $("div.button1").unbind("click")
+    $("li.button1").unbind("click")
     end()
   });
 
-  $("div.button2").click(function () {
-    let answer = prompt("For $100: 10 divided by zero")
+  $("li.button2").click(function () {
+    let answer = prompt("For 100: What is 10 divided by 0 ?")
     if (answer === '0') {
       alert("Good Job")
       newScore += 100
@@ -40,13 +40,13 @@ $(document).ready(function () {
       newScore -= 100
     }
     $(".score").html(newScore);
-    $("div.button2").unbind("click");
+    $("li.button2").unbind("click");
     
   });
 
-  $("div.button3").click(function () {
-    let answer = prompt("For $200: Fifty divided by forty, gives a remainder of?")
-    if (answer === '1') {
+  $("li.button3").click(function () {
+    let answer = prompt("For 200: Whats 25 + 2?")
+    if (answer === '27') {
       alert("Good Job");
       newScore += 200
     } else {
@@ -54,12 +54,12 @@ $(document).ready(function () {
       newScore -= 200
     }
     $(".score").html(newScore);
-    $("div.button3").unbind("click");
+    $("li.button3").unbind("click");
   });
 
-  $("div.button4").click(function () {
-    let answer = prompt("For $200: How many solutions are there to the equation x2 - 7 = 0?")
-    if (answer === '1') {
+  $("li.button4").click(function () {
+    let answer = prompt("For 200: What's 7 - 3 ? ")
+    if (answer === '4') {
       alert("Good Job");
       newScore += 200
     } else {
@@ -67,12 +67,12 @@ $(document).ready(function () {
       newScore -= 200
     }
     $(".score").html(newScore);
-    $("div.button4").unbind("click");
+    $("li.button4").unbind("click");
   });
 
-  $("div.button5").click(function () {
-    let answer = prompt("For $500: A square patch of grass has a perimeter of 52 meters. How long is each side of the patch?")
-    if (answer === '13') {
+  $("li.button5").click(function () {
+    let answer = prompt("For 500: What's 7 x 7 ")
+    if (answer === '49') {
       alert("Good Job");
       newScore += 500
     } else {
@@ -80,12 +80,12 @@ $(document).ready(function () {
       newScore -= 500
     }
     $(".score").html(newScore);
-    $("div.button5").unbind("click");
+    $("li.button5").unbind("click");
   });
 
-  $("div.button6").click(function () {
-    let answer = prompt("For $500: How many solutions are there to the equation x2 - 7 = 0?")
-    if (answer === '1') {
+  $("li.button6").click(function () {
+    let answer = prompt("For 500: How many tires does a 18 wheeler have? ")
+    if (answer === '18') {
       alert("Good Job");
       newScore += 500
     } else {
@@ -93,12 +93,12 @@ $(document).ready(function () {
       newScore -= 500
     }
     $(".score").html(new Score);
-    $("div.button6").unbind("click");
+    $("li.button6").unbind("click");
   });
 
-  $("div.button7").click(function () {
-    let answer = prompt("For $1000: How many solutions are there to the equation 15 +3 -1 / 7 = 0?")
-    if (answer === '1') {
+  $("li.button7").click(function () {
+    let answer = prompt("For 1000: What's how many points are in a dozen")
+    if (answer === '12') {
       alert("Good Job");
       newScore += 1000
     } else {
@@ -106,12 +106,12 @@ $(document).ready(function () {
       newScore -= 1000
     }
     $(".score").html(newScore);
-    $("div.button7").unbind("click");
+    $("li.button7").unbind("click");
   });
 
-  $("div.button8").click(function () {
-    let answer = prompt("For $1000: How many last names?")
-    if (answer === '1') {
+  $("li.button8").click(function () {
+    let answer = prompt("For 1000: Whats 5 + ( 3 x 5 )")
+    if (answer === '20') {
       alert("Good Job");
       newScore += 1000
     } else {
@@ -119,7 +119,7 @@ $(document).ready(function () {
       newScore -= 1000
     }
     $(".score").html(newScore);
-    $("div.button8").unbind("click");
+    $("li.button8").unbind("click");
   })  
 })
 
